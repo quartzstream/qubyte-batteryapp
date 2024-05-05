e2a43888cbcdd89c3c0a7777f8a8c1753240a3d2
QUbyte SOFT <qubytesoft@gmail.com>
2024-05-05 22:57:41 +0530

    batteryapp-webapp commit
    batteryapp-webapp project commit 1

---
+++ batteryapp-webapp/src/main/java/com/batteryapp/webapp/BatteryappWebappApplication.java
@@ -0,0 +1,13 @@
+package com.batteryapp.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryappWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatteryappWebappApplication.class, args);
+	}
+
+}
