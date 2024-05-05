9d7f3090ef054b5bc35579ed63d1ec5a941eb9de
QUbyte SOFT <qubytesoft@gmail.com>
2024-05-05 22:56:29 +0530

    batteryapp-services-mobile commit
    batteryapp-services-mobile project commit 1

---
+++ batteryapp-services-mobile/src/main/java/com/batteryapp/services/mobile/BatteryappServicesMobileApplication.java
@@ -0,0 +1,13 @@
+package com.batteryapp.services.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryappServicesMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatteryappServicesMobileApplication.class, args);
+	}
+
+}
---
+++ batteryapp-services-mobile/src/test/java/com/batteryapp/services/mobile/BatteryappServicesMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batteryapp.services.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatteryappServicesMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
